74dfe12ac79bee7b7422fd3d1a0771f0a64297df
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-02-10 09:17:07 -0800

    enabled raw mode to get inputs without having to press enter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{Event, KeyCode, KeyEvent, KeyModifiers, read},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand, Result
+    ExecutableCommand, Result, 
+    cursor::{MoveTo, Show, Hide}
 };
 use std::{
     error::Error,
@@ -19,8 +20,10 @@ fn main() -> Result<()> {
         stdout(),
         SetForegroundColor(Color::Blue),
         SetBackgroundColor(Color::Red),
+        Hide,
         EnterAlternateScreen,
     )?;
+    enable_raw_mode()?;
 
     let board = Board::new(vec![ // TODO: turn this into a macro
         'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', 
@@ -36,18 +39,22 @@ fn main() -> Result<()> {
     let ticks = Duration::from_millis(10);
 
     loop {
-        if crossterm::event::poll(ticks)? {
-            if let Event::Key(key) = event::read()? {
-                if let KeyCode::Char('q') = key.code {
-                    execute!(
-                        stdout(),
-                        LeaveAlternateScreen,
-                        ResetColor
-                    )?;
-                    return Ok(());
-                }
-            }
+        execute!(stdout(), MoveTo(0, 0))?;
+        match read()? {
+            Event::Key(KeyEvent{
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::NONE, ..
+            }) => break,
+            _ => (),
         }
     }
 
+    disable_raw_mode()?;
+    execute!(
+        stdout(),
+        LeaveAlternateScreen,
+        Show,
+        ResetColor
+    )?;
+    return Ok(());
 }
