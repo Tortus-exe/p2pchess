c0fa2d3a92942e253a4d4aafe0545d6cec57b915
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-02-09 16:35:03 -0800

    fixed main function to represent the new depends

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,26 @@ use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result
 };
 use std::{
     error::Error,
-    io,
+    io::{stdout, Write},
     time::{Duration, Instant},
-};
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    widgets::{BarChart, Block, Borders},
-    Frame, Terminal,
 };
 mod chessboard;
 use crate::chessboard::Board;
 
 // don't change this, it works well.
-fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+fn main() -> Result<()> {
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        EnterAlternateScreen,
+    )?;
 
-    // create app and run it
-    let tick_rate = Duration::from_millis(250);
     let board = Board::new(vec![ // TODO: turn this into a macro
         'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', 
         'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 
@@ -39,65 +32,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 
         'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'
     ]);
-    let res = run_app(&mut terminal, board, tick_rate);
 
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
+    let ticks = Duration::from_millis(10);
 
-    if let Err(err) = res {
-        println!("{:?}", err)
-    }
-
-    Ok(())
-}
-
-fn run_app<B: Backend>(terminal: &mut Terminal<B>,
-                        mut board: Board,
-                        tick_rate: Duration,
-                      ) -> io::Result<()> {
-    let mut last_tick = Instant::now();
     loop {
-        terminal.draw(|f| ui(f, &board))?;
-
-        let timeout = tick_rate
-            .checked_sub(last_tick.elapsed())
-            .unwrap_or_else(|| Duration::from_secs(0));
-        if crossterm::event::poll(timeout)? {
+        if crossterm::event::poll(ticks)? {
             if let Event::Key(key) = event::read()? {
                 if let KeyCode::Char('q') = key.code {
+                    execute!(
+                        stdout(),
+                        LeaveAlternateScreen,
+                        ResetColor
+                    )?;
                     return Ok(());
                 }
             }
         }
-        /* <-- timing thing
-        if last_tick.elapsed() >= tick_rate {
-            app.on_tick();
-            last_tick = Instant::now();
-        }
-        */
     }
-}
 
-fn white_square() -> Block<'static> {
-    Block::default()
-        .style(Style::default().bg(Color::White)) }
-fn black_square() -> Block<'static> {
-    Block::default().style(Style::default().bg(Color::Black))
-}
-
-fn ui<B: Backend>(f: &mut Frame<B>, board: &Board) {
-    let board_layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Ratio(1,8), Constraint::Ratio(1,8)].as_ref())
-        .split(f.size());
-//    let white_square = Block::default().style(Style::default().bg(Color::White));
-//    let black_square = Block::default().style(Style::default().bg(Color::Black));
-    f.render_widget(white_square(), board_layout[0]);
-    f.render_widget(black_square(), board_layout[0]);
 }
