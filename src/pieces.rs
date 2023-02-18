2b828027b7e6af42e9982cf52407b94791122080
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-02-17 16:51:53 -0800

    added pawn canMove verification

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,64 +1,48 @@
-type Square = Square;
+type Square = (u8,u8);
 
-struct pawn   {pos: Square, isWhite: bool, id: u8, displayChar: char}
-struct queen  {pos: Square, isWhite: bool, id: u8, displayChar: char}
-struct king   {pos: Square, isWhite: bool, id: u8, displayChar: char}
-struct rook   {pos: Square, isWhite: bool, id: u8, displayChar: char}
-struct knight {pos: Square, isWhite: bool, id: u8, displayChar: char}
-struct bishop {pos: Square, isWhite: bool, id: u8, displayChar: char}
+struct pawn   {pos: Square, isWhite: bool, displayChar: char}
+struct queen  {pos: Square, isWhite: bool, displayChar: char}
+struct king   {pos: Square, isWhite: bool, displayChar: char}
+struct rook   {pos: Square, isWhite: bool, displayChar: char}
+struct knight {pos: Square, isWhite: bool, displayChar: char}
+struct bishop {pos: Square, isWhite: bool, displayChar: char}
 
 pub trait chessPiece {
     pub fn getPosition(&self) -> Square;
-    pub fn requestMoveTo(&self, &board: Board) -> bool;
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square>
+    pub fn requestMoveTo(&self, pos: Square, board: &Board) -> bool;
+    pub fn canMoveTo(&self, pos: &Square, board: &Board) -> bool;
 }
-
 
 impl chessPiece for pawn {
     fn getPosition(&self) -> Square {self.pos}
-    fn requestMoveTo(&self, &board: Board) -> bool {
-        
+    fn requestMoveTo(&self, &(tx, ty): &Square, &board: &Board) -> bool {
+        true
     }
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square> {
-        if self.isWhite {
-            if board.get_at(self.pos).id == 0 {
-                //copy from old code;
+    fn canMoveTo(&self, &(tx, ty): &Square, &board: &Board) -> bool {
+        if board.get_at((tx, ty)) == None {
+            if self.isWhite {
+                return( ty < 7 &&
+                        tx == self.pos.0 &&
+                        (ty+1 == self.pos.1 ||
+                        (ty==4 && self.pos.1==6)));
+            } else {
+                return( ty > 0 && ty < 8 &&
+                        tx == self.pos.0 &&
+                        (ty-1 == self.pos.1 || 
+                        (ty==3 && self.pos.1==1)));
             }
         }
+        //white and taking
+        if self.isWhite {
+            return( ty < 7 &&
+                    (tx == self.pos.0+1 || 
+                     tx+1==self.pos.0) &&
+                    ty+1== self.pos.1);
+        } else {
+            return( ty > 0 && ty < 8 &&
+                    (tx == self.pos.0+1 ||
+                     tx+1==self.pos.0) &&
+                    ty==self.pos.1+1);
+        }
     }
-}
-impl chessPiece for queen {
-    fn getPosition(&self) -> Square {self.pos}
-    fn requestMoveTo(&self, &board: Board) -> bool {
-
-    }
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square>
-}
-impl chessPiece for king {
-    fn getPosition(&self) -> Square {self.pos}
-    fn requestMoveTo(&self, &board: Board) -> bool {
-
-    }
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square>
-}
-impl chessPiece for rook {
-    fn getPosition(&self) -> Square {self.pos}
-    fn requestMoveTo(&self, &board: Board) -> bool {
-
-    }
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square>
-}
-impl chessPiece for knight {
-    fn getPosition(&self) -> Square {self.pos}
-    fn requestMoveTo(&self, &board: Board) -> bool {
-
-    }
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square>
-}
-impl chessPiece for bishop {
-    fn getPosition(&self) -> Square {self.pos}
-    fn requestMoveTo(&self, &board: Board) -> bool {
-
-    }
-    pub fn validMoves(&self, &board: Board) -> Iterator<Item=Square>
 }
